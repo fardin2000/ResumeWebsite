e84bf7e0d1a4ea4d7ada752a1ddfda394f1056ee
Fardin Safikhani <safikhanifardin@gmail.com>
2020-07-01 15:58:15 -0400

    fix download

--- src/Components/About.js
+++ src/Components/About.js
@@ -48,7 +48,12 @@ class About extends Component {
               </div>
               <div className="columns download">
                 <p>
-                  <a href={resumeDownload} target="_blank" className="button">
+                  <a
+                    href={resumeDownload}
+                    target="_blank"
+                    className="button"
+                    download
+                  >
                     <i className="fa fa-download" />
                     Download Resume
                   </a>
